9c5ee287a73b894e6ed1057500af1b552e4edcd4
DESKTOP-NS\NShome <sounaka0203@ezweb.ne.jp>
2023-10-12 00:51:07 +0900

    プロジェクト ファイルを追加します。

---
+++ LifeGame.cs
@@ -0,0 +1,104 @@
+﻿namespace LifeGame
+{
+    internal class LifeGame
+    {
+        private int heightSize;
+        private int widthSize;
+
+        /// <summary>
+        /// 座標のcellの生存状態
+        /// </summary>
+        public bool[,] isAliveCell;
+
+        /// <summary>
+        /// 全体の大きさを決めるコンストラクタ
+        /// </summary>
+        /// <param name="heightSize">縦</param>
+        /// <param name="widthSize">横</param>
+        public LifeGame(int heightSize, int widthSize)
+        {
+            this.heightSize = heightSize;
+            this.widthSize = widthSize;
+
+            isAliveCell = new bool[heightSize, widthSize];
+        }
+
+        /// <summary>
+        /// cellの生死を判定する
+        /// </summary>
+        /// <param name="height">縦の座標</param>
+        /// <param name="width">横の座標</param>
+        /// <returns>生きているならTrue</returns>
+        private bool IsAlive(int height, int width)
+        {
+            int LifeCount = 0;
+
+            int left = width == 0 ? 0 : width - 1;
+            int right = width == widthSize - 1 ? widthSize - 1 : width + 1;
+
+            int under = height == 0 ? 0 : height - 1;
+            int up = height == heightSize - 1 ? heightSize - 1 : height + 1;
+
+            // 自分含めた周囲9マスのcellの生存状態を確認する
+            for (int x = left; x <= right; x++)
+            {
+                for (int y = under; y <= up; y++)
+                {
+                    if (!isAliveCell[y, x])
+                    {
+                        continue;
+                    }
+
+                    LifeCount++;
+                }
+            }
+
+            if (isAliveCell[height, width])
+            {
+                // 周囲8マスの生存cell数
+                int aroundLifeCount = LifeCount - 1;
+                // 周囲の生きているcellが 2<=x<=3 ならば生存
+                return aroundLifeCount >= 2 && aroundLifeCount <= 3;
+            }
+            else
+            {
+                // 自分が死んでいて周囲の生きているcell数が3つならば誕生
+                return LifeCount == 3;
+            }
+        }
+
+        /// <summary>
+        /// 世代を１つ進める
+        /// </summary>
+        /// <returns>次世代のcellの状態</returns>
+        public bool[,] NextGeneration()
+        {
+            bool[,] newGene = new bool[heightSize, widthSize];
+            for (int i = 0; i < heightSize; i++)
+            {
+                for (int j = 0; j < widthSize; j++)
+                {
+                    newGene[i, j] = IsAlive(i, j);
+                }
+            }
+            return newGene;
+        }
+
+        /// <summary>
+        /// 渡されたcellの状態を描画する
+        /// 生存:■ 死亡:□
+        /// </summary>
+        /// <param name="cells"></param>
+        public void RenderState()
+        {
+            for (int i = 0; i < heightSize; i++)
+            {
+                for (int j = 0; j < widthSize; j++)
+                {
+                    Console.Write(isAliveCell[i, j] ? '■' : '□');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace LifeGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height;
+            int width;
+            int generationCount = 0;
+
+            (width, height) = InitSetting();
+            int[,] init = new int[height, width];
+
+            Random random = new Random();
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    // ランダムに0,1を代入する
+                    init[i, j] = random.Next(2);
+                }
+            }
+
+            LifeGame lifeGame = new LifeGame(height, width);
+            lifeGame.isAliveCell = Int2Bool(init);
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"{generationCount}世代目");
+                lifeGame.RenderState();
+                Thread.Sleep(700);
+                lifeGame.isAliveCell = lifeGame.NextGeneration();
+                generationCount++;
+            }
+        }
+
+        /// <summary>
+        /// 初期設定
+        /// </summary>
+        /// <returns>(width,height)</returns>
+        static (int, int) InitSetting()
+        {
+            int width;
+            int height;
+            bool parseResult;
+
+            Console.Write("横の長さは？：");
+            parseResult = int.TryParse(Console.ReadLine(), out width);
+            if (!parseResult)
+            {
+                Console.WriteLine("数字を入力してね！");
+                return InitSetting();
+            }
+
+            Console.Write("縦の高さは？：");
+            parseResult = int.TryParse(Console.ReadLine(), out height);
+            if (!parseResult)
+            {
+                Console.WriteLine("数字を入力してね！");
+                return InitSetting();
+            }
+
+            return (width, height);
+        }
+
+        static bool[,] Int2Bool(int[,] ints)
+        {
+            int heightLength = ints.GetLength(0);
+            int widthLength = ints.GetLength(1);
+            bool[,] cellStates = new bool[heightLength, widthLength];
+
+            for (int i = 0; i < heightLength; i++)
+            {
+                for (int j = 0; j < widthLength; j++)
+                {
+                    // 0,1以外が入っていたらエラーを吐く
+                    if (!(ints[i, j] == 0 || ints[i, j] == 1)) throw new ArgumentException();
+
+                    cellStates[i, j] = ints[i, j] == 1;
+                }
+            }
+            return cellStates;
+        }
+    }
+}
